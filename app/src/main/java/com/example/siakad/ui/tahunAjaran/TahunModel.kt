379a2a57d16f24825ec630f60e86332ef44154e3
AnandaFatliah <ftlhhananda@gmail.com>
2023-12-29 14:54:16 +0800

    perbaikan import

--- app/src/main/java/com/example/siakad/ui/tahunAjaran/TahunModel.kt
+++ app/src/main/java/com/example/siakad/ui/tahunAjaran/TahunModel.kt
@@ -1,5 +1,8 @@
 package com.example.siakad.ui.tahunAjaran
 
+import com.example.siakad.ui.Links
+import com.example.siakad.ui.Meta
+
 data class TahunModel(
     val items: List<Tahun>,
     val _links: Links,
