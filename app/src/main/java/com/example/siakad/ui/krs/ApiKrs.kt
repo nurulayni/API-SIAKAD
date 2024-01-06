f85eeba9dc9a3b49ba6096e97137709a58caf68c
Dian <diaan18083@gmail.com>
2024-01-06 17:10:42 +0800

    daftar krs & daftar matakuliahKrs

--- app/src/main/java/com/example/siakad/ui/krs/ApiKrs.kt
+++ app/src/main/java/com/example/siakad/ui/krs/ApiKrs.kt
@@ -1,6 +1,7 @@
 package com.example.siakad.ui.krs
 
 import com.example.siakad.ui.mahasiswa.MahasiswaModel
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,4 +12,6 @@ interface ApiKrs {
 
     @GET("detail-krs")
     fun getMatakuliahKrs(@Query("id_krs") id_krs: Int): Call<MatakuliahModel>
+
+    fun deleteMatakuliahKrs(@Query("id") id: Int): Call<ResponseBody>
 }
--- app/src/main/java/com/example/siakad/ui/krs/DaftarKrs.kt
+++ app/src/main/java/com/example/siakad/ui/krs/DaftarKrs.kt
@@ -3,11 +3,14 @@ package com.example.siakad.ui.krs
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.siakad.R
 import com.example.siakad.ui.Config
+import com.example.siakad.ui.gallery.FetchJurusan
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -40,8 +43,15 @@ class DaftarKrs : AppCompatActivity() {
         recyclerView = findViewById<RecyclerView>(R.id.listMatakuliahKrs)
         adapter = MatakuliahAdapter(matakuliahList, object : MatakuliahAdapter.OnItemClickListener{
             override fun click(matakuliah: Matakuliah) {
-                val  intent= Intent(applicationContext, DaftarKrs::class.java)
-                startActivity(intent)
+                AlertDialog.Builder(applicationContext)
+                    .setTitle("Perhatian!!")
+                    .setMessage("Yakin hapus "+matakuliah.matakuliah.nama+" ?")
+                    .setPositiveButton("Ok") {_, _ ->
+                        deleteMatakuliah(matakuliah)
+                    }
+                    .setNegativeButton("Cancel") {_, _ ->
+
+                    }.show()
             }
         })
         val layoutManager = LinearLayoutManager(applicationContext)
@@ -50,7 +60,24 @@ class DaftarKrs : AppCompatActivity() {
         swipeRefreshLayout.isRefreshing = true
         fetchData()
     }
+    fun deleteMatakuliah(matakuliah: Matakuliah) {
+        val retrofi = Retrofit.Builder()
+            .baseUrl(Config.path)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        var apiKrs: ApiKrs
+        apiKrs = retrofi.create(ApiKrs::class.java)
+        var call: Call<ResponseBody>? = null
+        call = apiKrs.deleteMatakuliahKrs(matakuliah.matakuliah.id)
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                fetchData()
+            }
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
 
+            }
+        })
+    }
     fun fetchData() {
         isLoading = true
         val fecth = Retrofit.Builder()
