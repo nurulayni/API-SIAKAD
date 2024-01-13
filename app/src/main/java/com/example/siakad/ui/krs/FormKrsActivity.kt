888f01cb5ca26942b2ee1e257464ff4fe89a2535
Dian <diaan18083@gmail.com>
2024-01-13 16:41:59 +0800

    new conflic

---
+++ app/src/main/java/com/example/siakad/ui/krs/FormKrsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.siakad.ui.krs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.siakad.R
+
+class FormKrsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_krs)
+    }
+}
