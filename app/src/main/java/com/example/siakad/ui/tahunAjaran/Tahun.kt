a9b39f7a199ef2ca930fe6fb2d0b7024534163d8
AnandaFatliah <ftlhhananda@gmail.com>
2024-01-13 14:57:07 +0800

    perubahan tipe data

--- app/src/main/java/com/example/siakad/ui/tahunAjaran/Tahun.kt
+++ app/src/main/java/com/example/siakad/ui/tahunAjaran/Tahun.kt
@@ -3,6 +3,6 @@ package com.example.siakad.ui.tahunAjaran
 data class Tahun(
     val id: Int,
     val tahun: Int,
-    val semester: String,
-    val status: String
+    val semester: Int,
+    val status: Int
 )
