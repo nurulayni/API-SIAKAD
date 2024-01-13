8e0c360920f3b32f05b80cb979eed4d45adc00e1
AnandaFatliah <ftlhhananda@gmail.com>
2024-01-13 13:51:52 +0800

    pengaturan adapter

--- app/src/main/java/com/example/siakad/ui/tahunAjaran/TahunAjaranAdapter.kt
+++ app/src/main/java/com/example/siakad/ui/tahunAjaran/TahunAjaranAdapter.kt
@@ -9,17 +9,21 @@ import com.example.siakad.R
 
 class TahunAjaranAdapter (private var tahunajarList: List<Tahun>, private val onItemClickListener: TahunAjaranAdapter.OnItemClickListener) : RecyclerView.Adapter<TahunAjaranAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TahunAjaranAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_jurusan, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_tahunajar, parent, false)
         return ViewHolder(view)
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tahun: TextView = itemView.findViewById(R.id.tahunajar)
+        val status: TextView = itemView.findViewById(R.id.status)
+        val semester: TextView = itemView.findViewById(R.id.semester)
     }
 
     override fun onBindViewHolder(holder: TahunAjaranAdapter.ViewHolder, position: Int) {
         val tahunajar = tahunajarList[position]
-
+        holder.tahun.text = tahunajar.tahun.toString()
+        holder.status.text = tahunajar.status
+        holder.semester.text = tahunajar.semester
     }
 
     override fun getItemCount(): Int {
