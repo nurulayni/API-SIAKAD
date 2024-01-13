873326c522b0fecc625ae6e03200e37bf39e07c6
AnandaFatliah <ftlhhananda@gmail.com>
2024-01-13 14:56:17 +0800

    Penambahan metode update, delete, create

--- app/src/main/java/com/example/siakad/ui/tahunAjaran/ApiTahunAjar.kt
+++ app/src/main/java/com/example/siakad/ui/tahunAjaran/ApiTahunAjar.kt
@@ -1,10 +1,22 @@
 package com.example.siakad.ui.tahunAjaran
 
+import com.example.siakad.ui.gallery.DataPost
+import okhttp3.ResponseBody
 import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ApiTahunAjar {
     @GET("tahun-ajaran")
     fun getTahunAjar(@Query("page") page: Int): Call<TahunModel>
+    @POST("tahun-ajarans")
+    fun postTahunAjar(@Body dataPost: Tahun): Call<ResponseBody>
+    @PUT("tahun-ajaran/update")
+    fun updateTahunAjar(@Query("id") id: Int, @Body dataPost: Tahun): Call<ResponseBody>
+    @DELETE("tahun-ajaran/delete")
+    fun deleteTahunAjar(@Query("id") id: Int): Call<ResponseBody>
 }
