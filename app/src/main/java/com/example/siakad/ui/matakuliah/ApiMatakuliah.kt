93a65a695d7075fbfc80a28cae6203f6e2eb770e
nurulayni <nurulayni2012@gmail.com>
2024-01-14 22:26:31 +0800

    new api matakuliah

--- app/src/main/java/com/example/siakad/ui/matakuliah/ApiMatakuliah.kt
+++ app/src/main/java/com/example/siakad/ui/matakuliah/ApiMatakuliah.kt
@@ -1,8 +1,13 @@
 package com.example.siakad.ui.matakuliah
 
 import com.example.siakad.ui.mahasiswa.MahasiswaModel
+import okhttp3.ResponseBody
 import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ApiMatakuliah {
@@ -10,4 +15,10 @@ interface ApiMatakuliah {
     fun getMatakuliah(@Query("page") page: Int): Call<MatakuliahModel>
     @GET("matakuliah")
     fun getListMatakuliah(@Query("id_jurusan") id_jurusan: Int, @Query("nama") nama: String): Call<MatakuliahModel>
+    @POST("matakuliahs")
+    fun postMatakuliah(@Body dataPost: PostMatakuliah) : Call<ResponseBody>
+    @PUT("matakuliah/update")
+    fun updateMatakuliah(@Query("id") id: Int, @Body dataPost: PostMatakuliah): Call<ResponseBody>
+    @DELETE("matakuliah/delete")
+    fun deleteMatakuliah(@Query("id") id: Int): Call<ResponseBody>
 }
