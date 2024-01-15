86038b91dcb5a12f44ff09bd0edf4d8f20fe14d1
IRfanRIfan <Irfanrifan285@gmail.com>
2024-01-15 23:54:10 +0800

    api post mahasiswa

--- app/src/main/java/com/example/siakad/ui/mahasiswa/ApiMahasiswa.kt
+++ app/src/main/java/com/example/siakad/ui/mahasiswa/ApiMahasiswa.kt
@@ -1,13 +1,17 @@
 package com.example.siakad.ui.mahasiswa
 
+import okhttp3.ResponseBody
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiMahasiswa {
     @GET("mahasiswas")
     fun getMahasiswa(@Query("page") page: Int): Call<MahasiswaModel>
-
     @GET("mahasiswas")
     fun mahasiswa(@Query("nim") nim: String, @Query("id_jurusan") id_jurusan: Int): Call<MahasiswaModel>
+    @POST("mahasiswas")
+    fun postMahasiswa(@Body dataPost: PostMahasiswa): Call<ResponseBody>
 }
