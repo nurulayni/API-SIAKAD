da7fe8c7acfc193912a8dbb95df264a829f5e3f5
IRfanRIfan <Irfanrifan285@gmail.com>
2024-01-15 23:58:05 +0800

    tambahan api mahasiswa

--- app/src/main/java/com/example/siakad/ui/mahasiswa/ApiMahasiswa.kt
+++ app/src/main/java/com/example/siakad/ui/mahasiswa/ApiMahasiswa.kt
@@ -3,8 +3,10 @@ package com.example.siakad.ui.mahasiswa
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ApiMahasiswa {
@@ -14,4 +16,9 @@ interface ApiMahasiswa {
     fun mahasiswa(@Query("nim") nim: String, @Query("id_jurusan") id_jurusan: Int): Call<MahasiswaModel>
     @POST("mahasiswas")
     fun postMahasiswa(@Body dataPost: PostMahasiswa): Call<ResponseBody>
+    @PUT("mahasiswa/update")
+    fun updateMahasiswa(@Query("id") id: Int, dataPost: PostMahasiswa): Call<ResponseBody>
+    @DELETE("mahasiswa/delete")
+    fun deleteMahasiswa(@Query("id") id: Int): Call<ResponseBody>
+
 }
